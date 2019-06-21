92eb5b275f85bc1eb7e5171d4db8d6bf1ba8d9f5
李洪伟 <17862806857@163.com>
2019-06-21 11:19:36 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <Tabs :tab-position="tabPosition" :stretch="true" class="tabs-style">
+    <TabPane label="首页">
+      <Shouye></Shouye>
+    </TabPane>
+    <TabPane :lazy="true" label="预约">2</TabPane>
+    <TabPane :lazy="true" label="活动">3</TabPane>
+    <TabPane :lazy="true" label="我的">4</TabPane>
+  </Tabs>
+</template>
+
+<script>
+import { Tabs, TabPane } from "element-ui";
+import Shouye from '@/components/Shouye';
+export default {
+  name: "app",
+  components: {
+    Tabs,
+    TabPane,
+    Shouye
+  },
+  data() {
+    return {
+      tabPosition: "bottom"
+    };
+  }
+};
+</script>
+
+<style>
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+.tabs-style {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+.el-tabs__content {
+  flex-grow: 1;
+}
+</style>
---
+++ src/components/Shouye.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="top-carousel">
+    <Carousel>
+      <CarouselItem v-for="item in 4" :key="item">
+        <MyImage :src="carouselSrc"></MyImage>
+        <!-- <img :src="carouselSrc" alt=""> -->
+      </CarouselItem>
+    </Carousel>
+
+  </div>
+</template>
+
+<script>
+import { Carousel, CarouselItem, Image } from "element-ui";
+
+export default {
+  name: "shouye",
+  components: {
+    Carousel,
+    CarouselItem,
+    MyImage: Image
+  },
+  data: function() {
+    return {
+      carouselSrc: require('../assets/carousel_img.png'),
+      
+    };
+  }
+};
+</script>
+
+<style>
+.top-carousel {
+}
+</style>
